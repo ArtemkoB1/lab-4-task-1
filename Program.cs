3a58b58f30ce03e0469e2aeef315b92e1b967322
Артем Біленький <artem06bil@gmail.com>
2024-10-25 09:08:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace lab_4_A_та_P
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[9];
+            Random rand = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 51);
+            }
+
+            Console.WriteLine("Вихідний масив: ");
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+
+
+
+            int product = 1;
+            bool hasMultipleOf5 = false;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 5 == 0 && array[i] != 0)
+                {
+                    product *= array[i];
+                    hasMultipleOf5 = true;
+                }
+            }
+
+
+            if (hasMultipleOf5 == true)
+            {
+                Console.WriteLine($"Добуток елементів, кратних до 5: {product}");
+            }
+            else
+            {
+                Console.WriteLine("У масиві немає елементів, кратних до 5.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
